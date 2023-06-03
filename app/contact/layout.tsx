783754c011c1c18eaf85cc2d6949041e88699928
yashguptaFE <yash.gupta@smallcase.com>
2023-06-04 01:40:29 +0530

    feat: Nested Layouts

---
+++ app/contact/layout.tsx
@@ -0,0 +1,8 @@
+export default function ContactLayout({children}: {
+    children: React.ReactNode;
+  }) {
+    return <div>
+        <div>Contact Layout</div>
+        <div>{children}</div>
+    </div>
+}
